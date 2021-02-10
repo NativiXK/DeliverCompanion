ed708f03b734c22e0116a163fccfe1be769e8915
NativiXK <mateuskonkol@gmail.com>
2021-02-10 07:35:56 -0300

    Implemented danfe key control in db and menu navigation

--- dbConnector.py
+++ dbConnector.py
@@ -2,8 +2,10 @@ import sqlite3, time
 
 class Connector:
 
-    def __init__(self) -> None:
-        self.__cursor = sqlite3.connect('danfes.db').cursor()
+    def __init__(self, db_name) -> None:
+        self.__conn = sqlite3.connect(db_name)
+        self.__cursor = self.__conn.cursor()
+        self.__create_table()
         
     @property
     def cursor(self) -> object:
@@ -11,7 +13,7 @@ class Connector:
 
     def __create_table(self) -> None:
         self.cursor.execute("""
-        SELECT count(name) FROM sqlite_master WHERE type='table' AND name='key'
+        SELECT count(name) FROM sqlite_master WHERE type='table' AND name='keys'
         """)
         if self.cursor.fetchone()[0] == 1:
             print("Table already exists")
@@ -19,17 +21,39 @@ class Connector:
             self.cursor.execute("""
             CREATE TABLE keys (
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                key TEXT NOT NULL,
+                key VARCHAR(44) NOT NULL,
                 registrado_em DATE NOT NULL
             );
             """)
 
-    def register(self, danfe_key):
+    def register(self, danfe_key : str):
         self.cursor.execute(f"""
         INSERT INTO keys (key, registrado_em) 
-        VALUES {danfe_key}, {time.time()}
+        VALUES ("{danfe_key}", {time.time()})
+        """)
+        self.__conn.commit()
+
+    def delete(self, danfe_key : str):
+        self.cursor.execute(f"""
+        DELETE FROM keys WHERE key="{danfe_key}";        
+        """)
+        self.__conn.commit()
+
+    def retrieve_all_keys(self):
+        self.cursor.execute("""
+        SELECT key FROM keys;
         """)
 
+        keys = self.cursor.fetchall()
+
+        return [i[0] for i in keys]
+
 if __name__ == "__main__":
-    db = Connector()
-    print(db)
+    db = Connector('danfes.db')
+    db.register("31210106347409006953550060009454751196026791")
+    db.register("31210106347409006953550060009454751196226791")
+    for i in db.retrieve_all_keys():
+        print(i)
+    db.delete("31210106347409006953550060009454751196026791")
+    db.delete("31210106347409006953550060009454751196226791")
+    
--- main.py
+++ main.py
@@ -1,2 +1,71 @@
 from delivery import Delivery
-from dbConnector import connector
+from dbConnector import Connector
+
+def menu():
+    print("""
+Menu:
+-------------
+        1 - Consultar
+        2 - Cadastrar
+        3 - Deletar
+        4 - Sair
+    """)
+    return int(input("Escolha uma opção: "))
+
+def menu_keys(keys):
+    if len(keys) == 0:
+        print("\nNenhuma chave cadastrada!\n")
+        return 0
+    str_keys = "Chaves registradas: \n-------------\n\t0 - Voltar"
+
+    for ind in range(len(keys)):
+        str_keys += f"\n\t{ind + 1} - {keys[ind]}"
+    
+    print(str_keys)
+    selecao = int(input("\nSelecione uma chave: "))
+    
+    try:
+        if selecao == 0:
+            return 0
+        else:
+            return keys[selecao - 1]
+    except Exception:
+        print("\nNão localizei esta chave, tente novamente!\n")
+        return menu_keys(keys)
+
+
+def main():
+    db = Connector('danfes.db')
+
+    while True:
+        choice = menu()
+        keys = []
+        #Check
+        if choice == 1:
+            keys = db.retrieve_all_keys()
+            key = menu_keys(keys)
+            #If the selected key is higher than 0
+            if key != 0:
+                print(Delivery(key).get_events())
+            else:
+                continue
+        #Register
+        elif choice == 2:
+            key = input("\nChave da DANFe: ")
+            if len(key) >= 36:
+                db.register(key)
+            else:
+                print("\nChave inválida!")
+                continue
+        #Delete
+        elif choice == 3:
+            keys = db.retrieve_all_keys()
+            key = menu_keys(keys)
+            #If the selected key is higher than 0
+            if key != 0:
+                db.delete(key)
+            else:
+                continue
+
+if __name__ == "__main__":
+    main()
